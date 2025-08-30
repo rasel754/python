82b7ebcd0d9929fe81b2b1afb0fab6343373155d
unknown <raselahmed73614@gmail.com>
2025-08-30 18:42:13 +0600

    variable in python

---
+++ variable.py
@@ -0,0 +1,19 @@
+age=67
+me_name="rasel"
+education ="engineer"
+basa="chandpur"
+# print(age , me_name, education , basa)
+# print(type(age)) #check type
+# print(type(education))
+
+
+""" 
+multi line comment
+ """
+
+""" Alt + shift + A    for multiline comment  """
+
+print("rasel"+" " + "ahmed shole")
+
+porichoy = f"my name is {me_name} i am from {basa} . i am {age} old"
+print(porichoy)
