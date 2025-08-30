a22eac8b99201a13e676fba9bc3c7bca4e28b5e8
unknown <raselahmed73614@gmail.com>
2025-08-30 21:00:00 +0600

    user input

---
+++ input.py
@@ -0,0 +1,25 @@
+money=input("give me some money: ")
+print("here is your monye: ",money)
+
+a=input("givem a ammount money: ")
+b=input("give b ammonut money: ")
+#by default input from user will be string 
+a_int =int(a)
+b_int =int(b)
+total=a_int+b_int
+print("total ammount you got ",total)
+
+
+
+
+# convert number to string 
+number = 1243
+number_in_string =str(number)
+# print(type(number_in_string))
+
+# convert decimal number in float
+string_decimal = "3.14159"
+pi = float(string_decimal) # Convert it to a float
+
+print(pi)          # Output: 3.14159
+print(type(pi))
