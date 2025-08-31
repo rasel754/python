1f38821643665a1d07e87fda47b47079183663d6
unknown <raselahmed73614@gmail.com>
2025-09-01 00:29:45 +0600

    set

---
+++ set.py
@@ -0,0 +1,9 @@
+#set: unique item collection . no duplicate 
+numbers=[12 ,23,34,32,34,12,89,98,89]
+
+print(numbers)
+numbers_set=set(numbers)
+print(numbers_set)
+
+numbers_set.add(45)
+print(numbers_set)
